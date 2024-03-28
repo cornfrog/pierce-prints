993dd320733e40523ad394346418e370a6e4c630
Cornfrog <iamcornfrog@gmail.com>
2024-03-28 08:54:03 -0400

    added welcome banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <div className="welcome-banner">
+      <h1 className="welcome-banner__title">Welcome!</h1>
+      <p className="welcome-banner__text">If you can think it. We can print it!</p>
+      <p className="welcome-banner__mission">
+        Welcome to Pierce Prints! Our mission is to provide top-quality
+        3D printed products to communities far and wide. Whether it's a
+        unique desk ornament, a key piece for your cosplay ensemble, or
+        anything else you can imagine, we're here to bring your ideas to
+        life. Can't find exactly what you're looking for? No problem!
+        Just drop us a message via our contact page, and we'll work to
+        fulfill your needs. Thank you for visiting us – we hope you enjoy
+        exploring our collection!
+      </p>
+    </div>
   );
 }
