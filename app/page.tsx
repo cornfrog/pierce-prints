fe4213e7e334c9e20c8cbe3a3c3843c492b22281
Cornfrog <iamcornfrog@gmail.com>
2024-03-28 16:10:05 -0400

    fixed welcome message text issue

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
         <p className="welcome-banner__text">If you can think it. We can print it!</p>
         <p className="welcome-banner__mission">
           Welcome to Pierce Prints! Our mission is to provide top-quality
-          3D printed products to communities far and wide. Whether it's a
+          3D printed products to communities far and wide. Whether it&apos;s a
           unique desk ornament, a key piece for your cosplay ensemble, or
-          anything else you can imagine, we're here to bring your ideas to
-          life. Can't find exactly what you're looking for? No problem!
-          Just drop us a message via our contact page, and we'll work to
+          anything else you can imagine, we&apos;re here to bring your ideas to
+          life. Can&apos;t find exactly what you&apos;re looking for? No problem!
+          Just drop us a message via our contact page, and we&apos;ll work to
           fulfill your needs. Thank you for visiting us – we hope you enjoy
           exploring our collection!
         </p>
@@ -20,6 +20,6 @@ export default function Home() {
         <h1 className="separator-container__text">Categories</h1>        
         <div className="separator-container__bar"></div>
       </div>
-    </>
+    </> 
   );
 }
