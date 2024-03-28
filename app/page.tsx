573d7a92cfef32e7dffa783ea002d5f0e13a15a5
Cornfrog <iamcornfrog@gmail.com>
2024-03-28 09:09:22 -0400

    added spacer for catagories on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
 export default function Home() {
   return (
-    <div className="welcome-banner">
-      <h1 className="welcome-banner__title">Welcome!</h1>
-      <p className="welcome-banner__text">If you can think it. We can print it!</p>
-      <p className="welcome-banner__mission">
-        Welcome to Pierce Prints! Our mission is to provide top-quality
-        3D printed products to communities far and wide. Whether it's a
-        unique desk ornament, a key piece for your cosplay ensemble, or
-        anything else you can imagine, we're here to bring your ideas to
-        life. Can't find exactly what you're looking for? No problem!
-        Just drop us a message via our contact page, and we'll work to
-        fulfill your needs. Thank you for visiting us – we hope you enjoy
-        exploring our collection!
-      </p>
-    </div>
+    <>
+      <div className="welcome-banner">
+        <h1 className="welcome-banner__title">Welcome!</h1>
+        <p className="welcome-banner__text">If you can think it. We can print it!</p>
+        <p className="welcome-banner__mission">
+          Welcome to Pierce Prints! Our mission is to provide top-quality
+          3D printed products to communities far and wide. Whether it's a
+          unique desk ornament, a key piece for your cosplay ensemble, or
+          anything else you can imagine, we're here to bring your ideas to
+          life. Can't find exactly what you're looking for? No problem!
+          Just drop us a message via our contact page, and we'll work to
+          fulfill your needs. Thank you for visiting us – we hope you enjoy
+          exploring our collection!
+        </p>
+      </div>
+      <div className="separator-container">
+        <div className="separator-container__bar"></div>
+        <h1 className="separator-container__text">Categories</h1>        
+        <div className="separator-container__bar"></div>
+      </div>
+    </>
   );
 }
