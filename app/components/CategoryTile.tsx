9b1e3e2dacbd0d51a8d9faadb33a143c1f226e4a
CornFrog <thecornfrog@gmail.com>
2024-04-01 15:20:21 -0400

    added category styles

--- app/components/CategoryTile.tsx
+++ app/components/CategoryTile.tsx
@@ -3,6 +3,6 @@ import { CategoryTileProps } from "../types";
 
 export default function CategoryTile({ category }: CategoryTileProps) {
     return (
-        <Link href={`/categories/${category.route}`}>{category.name}</Link>
+        <Link href={`/categories/${category.route}`} className="category-link">{category.name}</Link>
     );
 }
