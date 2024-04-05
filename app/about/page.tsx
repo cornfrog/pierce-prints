a8d0b77ecaa39ec2816bdb0fbe90a7a9451b2c50
Cornfrog <iamcornfrog@gmail.com>
2024-04-04 23:11:03 -0400

    resolve vercel errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,24 +4,24 @@ export default function About() {
       <h1 className="about-title">About Us</h1>
       <div className="about-message">
         <p>
-          Welcome to Pierce Prints, where I'm dedicated
+          Welcome to Pierce Prints, where I&apos;m dedicated
           to providing access to high-quality 3D
           prints for all your imaginative creations!
         </p>
         <p>
-          Hey there, fellow tech enthusiasts! I'm passionate
+          Hey there, fellow tech enthusiasts! I&apos;m passionate
           about turning your wildest ideas into tangible
           objects through 3D printing.
         </p>
         <p>
           I started this site because I believe everyone should
           have access to top-notch 3D prints for bringing their
-          imagination to life. So if you're searching for premium
-          prints to make your dreams a reality, you've found the
+          imagination to life. So if you&apos;re searching for premium
+          prints to make your dreams a reality, you&apos;ve found the
           perfect destination!
         </p>
         <p>
-          Let's embark on this creative journey together at Pierce Prints!
+          Let&apos;s embark on this creative journey together at Pierce Prints!
         </p>
       </div>
     </>
